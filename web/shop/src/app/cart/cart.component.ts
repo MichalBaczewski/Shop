9adf7e8bcca6173c93d172eca235b4251d3c58b1
MichalBaczewski <michal.baczewski@outlook.com>
2018-12-16 14:42:04 +0100

    Dodano obsługę koszyka na froncie.

---
+++ web/shop/src/app/cart/cart.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {CartModel} from "../shared/model/cart.model";
+import {CartService} from "../shared/service/cart.service";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cart: CartModel = new CartModel();
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    this.cartService.getCartStream().subscribe((cart: CartModel) => {
+      this.cart = cart;
+    });
+  }
+  isEmptyCart(): boolean {
+    return this.cartService.isEmpty();
+  }
+
+  updateCart() {
+    this.cartService.updateCart(this.cart);
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
+  }
+
+  cartSummary(): number {
+    return this.cartService.summary();
+  }
+
+  removeFromCart(warehouseItemId: number) {
+    console.log(warehouseItemId);
+    this.cartService.removeItem(warehouseItemId);
+  }
+
+
+}
---
+++ web/shop/src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {LoginUserModel} from "../shared/model/login-user.model";
+import {AuthService} from "../shared/service/auth.service";
+import {AppService} from "../shared/service/app.service";
+import {Location} from "@angular/common";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userData: LoginUserModel = new LoginUserModel();
+
+  constructor(private authService: AuthService,
+              private location: Location,
+              private appService: AppService) { }
+
+  ngOnInit() {
+    this.appService.getLoggedUserStream().subscribe(() => {
+      if (this.appService.isLoggedIn()) {
+        this.location.back();
+      }
+    });
+  }
+
+  login() {
+    this.authService.login(this.userData).subscribe((data) => {
+      this.back();
+    });
+  }
+
+  back() {
+    this.location.back();
+  }
+
+}
